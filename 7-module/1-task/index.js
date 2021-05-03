a7fcd82e7f41414833e2d8d47b38550a3b28eb92
Bakhtiyar Zubairov <murderer187@mail.ru>
2021-05-03 18:22:13 +0200

    Add solution for 7 module task 1

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,112 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.elem = null;
+    this._render();
+    this._getAllElements();
+    this._listeners();
+  }
+
+  _getAllElements() {
+    this._ribbonInner = this.elem.querySelector(".ribbon__inner");
+    this._ribbonItems = this.elem.querySelectorAll(".ribbon__item");
+    this._ribbonArrowLeft = this.elem.querySelector(".ribbon__arrow_left");
+    this._ribbonArrowRight = this.elem.querySelector(".ribbon__arrow_right");
+  }
+
+  _listeners() {
+    this._ribbonInner.addEventListener("click", this._onClickRibbonInner);
+
+    this._ribbonArrowLeft.addEventListener("click", this._onMoveRibbonToRight);
+    this._ribbonArrowRight.addEventListener("click", this._onMoveRibbonToLeft);
+  }
+
+  _onMoveRibbonToRight = () => {
+    this._ribbonInner.scrollBy(-350, 0);
+
+    if (this._scrollLeft() < 1) {
+      this._ribbonInner.addEventListener("scroll", () => {
+          this._ribbonArrowLeft.classList.remove("ribbon__arrow_visible");
+        },
+        { once: true }
+      );
+
+      return;
+    }
+    this._showAllArrows();
+  };
+
+  _onMoveRibbonToLeft = () => {
+    this._ribbonInner.scrollBy(350, 0);
+
+    if (this._scrollRight() < 1) {
+      this._ribbonInner.addEventListener("scroll", () => {
+          this._ribbonArrowRight.classList.remove("ribbon__arrow_visible");
+        },
+        { once: true }
+      );
+
+      return;
+    }
+    this._showAllArrows();
+  };
+
+  _showAllArrows() {
+    this._ribbonArrowRight.classList.add("ribbon__arrow_visible");
+    this._ribbonArrowLeft.classList.add("ribbon__arrow_visible");
+  }
+
+  _scrollWidth = () => this._ribbonInner.scrollWidth;
+  _clientWidth = () => this._ribbonInner.clientWidth;
+  _scrollLeft = () => this._ribbonInner.scrollLeft;
+  _scrollRight = () => this._scrollWidth() - this._scrollLeft() - this._clientWidth();
+
+  _onClickRibbonInner = (e) => {
+    e.preventDefault();
+
+    const ribbonItem = e.target.closest(".ribbon__item");
+    if(!ribbonItem) {
+
+      return;
+    }
+    this._ribbonItems.forEach((item) => item.classList.remove("ribbon__item_active"));
+    ribbonItem.classList.add("ribbon__item_active");
+
+    const customEvent = new CustomEvent("ribbon-select", {
+      detail: ribbonItem.dataset.id,
+      bubbles: true,
+    });
+
+    this.elem.dispatchEvent(customEvent);
+  };
+
+  _render() {
+    this.elem = createElement(
+      `
+      <div class="ribbon">
+      <button class="ribbon__arrow ribbon__arrow_left">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+      <nav class="ribbon__inner">
+      ${this._getLinks()}
+      </nav>
+      <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    </div>
+      `
+    );
+  }
+
+  _getLinks() {
+    const layout = this.categories
+      .map(
+        (link) =>
+          `<a href="#" class="ribbon__item" data-id="${link.id}">${link.name}</a>`
+      )
+      .join("");
+    return layout;
   }
 }
